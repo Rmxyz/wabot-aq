6263b3c5b6683f4af79e4705f1a0ae904e9be97b
Rmxyz <78423844+Rmxyz@users.noreply.github.com>
2021-10-26 13:41:59 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,17 +1,22 @@
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-module.exports = {
-  before(m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return true
-    let chat = global.db.data.chats[m.chat]
-    let isGroupLink = linkRegex.exec(m.text)
+let handler = m => m
 
-    if (chat.antiLink && isGroupLink) {
-      m.reply('Hapus!!\n\nLink Grup terdeteksi')
-      if (global.opts['restrict']) {
-        if (isAdmin || !isBotAdmin) return true
-        // this.groupRemove(m.chat, [m.sender])
-      }
-    }
-    return true
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
+
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*「 ANTI LINK 」*\n\nTerdeteksi *${await this.getName(m.sender)}* kamu telah mengirim link group!\n\nMaaf Kamu akan dikick dari grup ini byee!`)
+    if (isAdmin) return m.reply('*Karena kamu admin, kamu gk bakal dikick*')
+    if (!isBotAdmin) return m.reply('*Jadikan Bot Admin Untuk Mengaktifkan Fitur ini*')
+    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
+    let isLinkThisGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkThisGc.test(m.text)
+    if (isgclink) return m.reply('*Lol ngirim link group sendiri :v*')
+    await this.groupRemove(m.chat, [m.sender])
   }
+  return true
 }
+
+module.exports = handler
